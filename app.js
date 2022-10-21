dccfc99402d97b6f572c0a54bbef6e06b1125e20
Alexandrovna <tatyanka.valchuk@gmail.com>
2022-10-21 14:14:14 +0300

    First commit 21/10/22 with create

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// let button1 = document.querySelector("#button1");
+
+// let urlCoordinates = "http://api.open-notify.org/iss-now.json";
+// let promiseCoordinates = fetch(urlCoordinates);
+
+// button1.addEventListener("click", function () {
+//     promiseCoordinates
+//     .then(response => response.json())
+//     .then(coordinates => {
+//         let latitude = coordinates.iss_position.latitude;
+//         let longitude = coordinates.iss_position.longitude;
+//         console.log(longitude + "</br>" + latitude);
+//     })
+   
+// })
+       
+//  
---
+++ map.js
@@ -0,0 +1,94 @@
+
+let latitude = 0;
+let longitude = 0;
+let myLatLng = {lat: latitude, lng: longitude};
+let people = [];
+let urlCoordinates = "http://api.open-notify.org/iss-now.json";
+let numberOfPeople = "http://api.open-notify.org/astros.json";
+let locationValueLat = document.querySelector("#locate span:first-child");
+let locationValueLng = document.querySelector("#locate span:nth-child(2)");
+
+let template = document.querySelector("#people-list").innerHTML;
+let peopleName = document.querySelector("#people-name");
+let peopleNumber = document.querySelector("#total");
+let sumPeople = 0;
+
+function getTime() {
+  let dateNow = new Date();
+  let hour = dateNow.getHours().toString().length < 2 ? '0' + dateNow.getHours() : dateNow.getHours();
+  let minut = dateNow.getMinutes().toString().length < 2 ? '0' + dateNow.getMinutes() : dateNow.getMinutes();
+  let timeNow = `${hour}:${minut}`;
+  let outputTime = document.querySelector("#time").innerHTML = timeNow;
+} 
+setInterval(getTime, 1000);
+function getDayToday () {
+  let dateNow = new Date();
+  let day = dateNow.getDay();
+  let month = dateNow.getMonth();  
+  let dayToday = document.querySelector("#day-today").innerHTML = `${day}, ${month}`;
+}
+getDayToday ();
+
+
+function getNumberOfPeople () {
+  fetch(numberOfPeople)
+    .then(response => response.json())
+    .then(peopleCraft => {
+      people = peopleCraft.people.filter(peopleInArr => peopleInArr.craft === "ISS");
+      return people;
+    })
+    .then(peopleCraft => {      
+      peopleCraft.forEach(el => {
+        let n = el;
+        let html = "";        
+        html = Mustache.render(template, n);
+        peopleName.insertAdjacentHTML("beforeend", html);       
+        sumPeople += 1;      
+      });
+      peopleNumber.textContent = sumPeople;      
+    }) 
+}
+
+function update () { 
+  peopleName.innerHTML = " ";
+  sumPeople = 0;
+  getCoordinates();
+  initMap();
+  getNumberOfPeople();
+}
+
+setInterval(update, 5000);
+
+function getCoordinates () {
+  fetch(urlCoordinates)
+    .then(response => response.json())
+    .then(coordinates => {
+      latitude = +(coordinates.iss_position.latitude);
+      longitude = +(coordinates.iss_position.longitude);
+      locationValueLat.textContent = latitude;
+      locationValueLng.textContent = longitude;      
+      console.log(latitude + "</br>" + longitude);
+      return myLatLng = {lat: latitude, lng: longitude};                   
+    })            
+  };
+      
+      
+function initMap() {
+  //myLatLng = {lat: latitude, lng: longitude};
+
+    const map = new google.maps.Map(
+    document.getElementById("map"), 
+    {
+      zoom: 4,
+      center: myLatLng,
+    });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: `latitude ${latitude} longitude ${longitude}`,
+  });
+}
+
+window.initMap = initMap;
+getNumberOfPeople ()
