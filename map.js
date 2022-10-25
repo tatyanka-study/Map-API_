fe3c6b1ca55e3428254a9eb7337689b8b5fc8bc2
Alexandrovna <tatyanka.valchuk@gmail.com>
2022-10-25 20:14:58 +0300

    update function getTime; comment less file in html

--- map.js
+++ map.js
@@ -1,8 +1,7 @@
-
 let latitude = 0;
 let longitude = 0;
 let marker;
-let myLatLng = {lat: latitude, lng: longitude};
+let myLatLng = { lat: latitude, lng: longitude };
 let people = [];
 let urlCoordinates = "http://api.open-notify.org/iss-now.json";
 let numberOfPeople = "http://api.open-notify.org/astros.json";
@@ -16,88 +15,92 @@ let sumPeople = 0;
 
 function getTime() {
   let dateNow = new Date();
-  let hour = dateNow.getHours().toString().length < 2 ? '0' + dateNow.getHours() : dateNow.getHours();
-  let minut = dateNow.getMinutes().toString().length < 2 ? '0' + dateNow.getMinutes() : dateNow.getMinutes();
+  let hour =
+    dateNow.getHours().toString().length < 2
+      ? "0" + dateNow.getHours()
+      : dateNow.getHours();
+  let minut =
+    dateNow.getMinutes().toString().length < 2
+      ? "0" + dateNow.getMinutes()
+      : dateNow.getMinutes();
   let timeNow = `${hour}:${minut}`;
-  let outputTime = document.querySelector("#time").innerHTML = timeNow;
-} 
+  document.querySelector("#time").innerHTML = timeNow;
+}
 setInterval(getTime, 1000);
 
-function getDayToday () {
+function getDayToday() {
   let dateNow = new Date();
   let day = dateNow.getDay();
-  let month = dateNow.getMonth();  
-  let dayToday = document.querySelector("#day-today").innerHTML = `${day}, ${month}`;
+  let month = dateNow.getMonth();
+  document.querySelector("#day-today").innerHTML = `${day}, ${month}`;
 }
-getDayToday ();
+getDayToday();
 
-
-function getNumberOfPeople () {
+function getNumberOfPeople() {
   fetch(numberOfPeople)
-    .then(response => response.json())
-    .then(peopleCraft => {
-      people = peopleCraft.people.filter(peopleInArr => peopleInArr.craft === "ISS");
+    .then((response) => response.json())
+    .then((peopleCraft) => {
+      people = peopleCraft.people.filter(
+        (peopleInArr) => peopleInArr.craft === "ISS"
+      );
       return people;
     })
-    .then(peopleCraft => {      
-      peopleCraft.forEach(el => {
+    .then((peopleCraft) => {
+      peopleCraft.forEach((el) => {
         let n = el;
-        let html = "";        
+        let html = "";
         html = Mustache.render(template, n);
-        peopleName.insertAdjacentHTML("beforeend", html);       
-        sumPeople += 1;      
+        peopleName.insertAdjacentHTML("beforeend", html);
+        sumPeople += 1;
       });
-      peopleNumber.textContent = sumPeople;      
-    }) 
+      peopleNumber.textContent = sumPeople;
+    });
 }
 
-function update () { 
+function update() {
   peopleName.innerHTML = " ";
   sumPeople = 0;
   getCoordinates().then(() => {
     marker.setPosition(myLatLng);
-    console.log('title' + latitude)
-    marker.setTitle(`latitude ${latitude} longitude ${longitude}`); 
-  });   
+    console.log("title" + latitude);
+    marker.setTitle(`latitude ${latitude} longitude ${longitude}`);
+  });
   getNumberOfPeople();
 }
 
 setInterval(update, 5000);
 
-function getCoordinates () {
+function getCoordinates() {
   return fetch(urlCoordinates)
-    .then(response => response.json())
-    .then(coordinates => {
-      latitude = +(coordinates.iss_position.latitude);
-      console.log('fetch' + latitude)
-      longitude = +(coordinates.iss_position.longitude);
+    .then((response) => response.json())
+    .then((coordinates) => {
+      latitude = +coordinates.iss_position.latitude;
+      console.log("fetch" + latitude);
+      longitude = +coordinates.iss_position.longitude;
       locationValueLat.textContent = latitude;
-      locationValueLng.textContent = longitude;  
-         
-      // console.log(latitude + "</br>" + longitude);
-      return myLatLng = {lat: latitude, lng: longitude};                   
-    })           
-  };
+      locationValueLng.textContent = longitude;
 
-  getCoordinates().then(() => {
-   initMap();
-  })
-      
+      // console.log(latitude + "</br>" + longitude);
+      return (myLatLng = { lat: latitude, lng: longitude });
+    });
+}
+
+getCoordinates().then(() => {
+  initMap();
+});
+
 function initMap() {
   //myLatLng = {lat: latitude, lng: longitude};
 
-   const map = new google.maps.Map(
-    document.getElementById("map"), 
-    {
-      zoom: 2,
-      center: myLatLng,
-    });
-  
-    marker = new google.maps.Marker({    
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: myLatLng,
+  });
+
+  marker = new google.maps.Marker({
     position: myLatLng,
     map,
     title: `latitude ${latitude} longitude ${longitude}`,
   });
 }
-
 getNumberOfPeople();
